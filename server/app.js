c9081f8d3230b2f7d087ed2ef5b056f1acb878fa
Ty Skelton <skeltont@oregonstate.edu>
2018-07-02 20:39:23 -0700

    adds mongodb integration

--- server/app.js
+++ server/app.js
@@ -1,43 +1,42 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 
-var zones = require('./routes/zones');
+const zones = require('./routes/zones')
+const app = express()
 
-var app = express();
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/zones', zones)
 
-app.use('/zones', zones);
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function (req, res, next) {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
--- server/routes/zones.js
+++ server/routes/zones.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const env = require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient
 
-router.get('/', function(req, res, next) {
-  res.status(200).json({
-    text: 'hey'
+router.get('/', function (req, res, next) {
+  var db = null
+  MongoClient.connect(env.parsed.mongo_url, (err, client) => {
+    if (err) return console.log(err)
+
+    db = client.db('campaign40k')
+    db.collection('zones').find().toArray(function (err, results) {
+      if (err) return console.log(err)
+      res.status(200).json({
+        zones: results
+      })
+    })
   })
-});
+})
 
-module.exports = router;
+module.exports = router
